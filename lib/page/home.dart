920bef8f9f0ed2c3c5ad6c13dfbe37f8ac1911c8
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-21 21:25:19 +0900

    feat: add botfriend image of homepage

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,13 +1,29 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  static const String BOYFRIEND_IMAGE_PATH = "assets/images/gym_rat_man.jpg";
+
+  Widget boyFriendButton() {
+    return InkWell(
+      onTap: () => Null,
+      child: Row(children: [
+        Image.asset(
+          BOYFRIEND_IMAGE_PATH,
+          width: 100,
+          height: 100,
+        )
+      ]),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('homepage'),
-    );
+    return Center(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [boyFriendButton()],
+    ));
   }
 }
