bbffbd2794f9edc1f00deb9efdf202fcee302307
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-22 15:57:37 +0900

    feat UI of homepage

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -4,26 +4,50 @@ import 'package:flutter/widgets.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   static const String BOYFRIEND_IMAGE_PATH = "assets/images/gym_rat_man.jpg";
+  static const String GIRLFRIEND_IMAGE_PATH = "assets/images/cuty_any_girl.jpg";
 
   Widget boyFriendButton() {
-    return InkWell(
+    return FittedBox(
+        child: InkWell(
       onTap: () => Null,
-      child: Row(children: [
+      child: Column(children: [
         Image.asset(
           BOYFRIEND_IMAGE_PATH,
           width: 100,
           height: 100,
-        )
+        ),
+        Text('나만 바라봐주는 남자친구'),
       ]),
-    );
+    ));
+  }
+
+  Widget girlfriendButton() {
+    return FittedBox(
+        child: InkWell(
+      onTap: () => Null,
+      child: Column(children: [
+        Image.asset(
+          GIRLFRIEND_IMAGE_PATH,
+          width: 100,
+          height: 100,
+        ),
+        Text('나만 바라봐주는 여자친구'),
+      ]),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [boyFriendButton()],
-    ));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('GirlFriend GPT'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: Center(
+                child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [boyFriendButton(), girlfriendButton()],
+        ))));
   }
 }
