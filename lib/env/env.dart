aba090c61243173c2fb385cecabb1f06118e1a8a
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-23 20:05:52 +0900

    refactor: fix varible name of env model

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,10 +1,9 @@
 // lib/env/env.dart
 import 'package:envied/envied.dart';
-
 part 'env.g.dart';
 
-@Envied()
+@Envied(path: ".env")
 abstract class Env {
-  @EnviedField(varName: 'KEY')
-  static const String key = _Env.key;
+  @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
+  static const String apiKey = _Env.apiKey;
 }
