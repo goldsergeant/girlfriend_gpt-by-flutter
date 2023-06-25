1d88291f8b165a7a948b1b7d7a0061e980468e1e
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-25 20:04:01 +0900

    feat: add user_profile_page

--- lib/page/user_profile.dart
+++ lib/page/user_profile.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:girlfriend_gpt/services/firebase_service.dart';
 
 class UserProfilePage extends StatelessWidget {
-  const UserProfilePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,12 +11,34 @@ class UserProfilePage extends StatelessWidget {
           image: AssetImage("assets/images/cherryblossom.gif"),
         )),
         child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: Text('마이페이지'),
-            centerTitle: true,
-          ),
-          body: Container(),
-        ));
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: Text('마이페이지'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage:
+                        AssetImage("assets/images/user_image.jpg"), // 프로필 이미지
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    FirebaseService.getUser()!.displayName ?? '닉네임을 입력해주세요',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // 프로필 편집 기능 구현
+                    },
+                    child: Text('Edit Profile'),
+                  ),
+                ],
+              ),
+            )));
   }
 }
