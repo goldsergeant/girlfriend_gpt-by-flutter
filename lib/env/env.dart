eb47cc86eb92a0e9b0d9359ec761d33aba473d1c
goldsergeant <rmagksfla000@gmail.com>
2023-08-02 15:15:19 +0900

    build: Add package about jwt authentication

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,8 +1,7 @@
-// lib/env/env.dart
 import 'package:envied/envied.dart';
 part 'env.g.dart';
 
-@Envied(path: ".env")
+@Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
   static const String apiKey = _Env.apiKey;
