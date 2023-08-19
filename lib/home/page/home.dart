a1151b4b18ecf522b308233cdf8489666f8f0733
goldsergeant <rmagksfla000@gmail.com>
2023-08-19 17:51:16 +0900

    fix: Fix import

--- lib/home/page/home.dart
+++ lib/home/page/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:girlfriend_gpt/home/page/character_list.dart';
 import 'package:girlfriend_gpt/home/page/user_profile.dart';
 import 'package:girlfriend_gpt/services/auth_service.dart';
-import 'before_chat.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
